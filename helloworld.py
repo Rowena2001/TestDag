5e0cea3438b0d52ed65dbfb1901b3c649967c1bb
Rowena2001 <rowshi2001@live.ca>
2023-07-10 16:28:39 +0000

    last changes

--- helloworld.py
+++ helloworld.py
@@ -1,20 +1,17 @@
+# file: helloworld.py
+# This file deploys a simple "hello world" function and a driver that calls it.
+# It follows the basic_dag.py example created by the ray-project team.
+# Refer to https://github.com/ray-project/test_dag for original source code.
+
 from ray import serve
 from ray.serve.deployment_graph import RayServeDAGHandle
 
-@serve.deployment(
-    ray_actor_options={
-        "num_cpus": 0.1,
-    }
-)
+@serve.deployment(ray_actor_options={"num_cpus": 0.1})
 def f(*args):
     return "hello world! \n"
 
 
-@serve.deployment(
-    ray_actor_options={
-        "num_cpus": 0.1,
-    }
-)
+@serve.deployment(ray_actor_options={"num_cpus": 0.1})
 class BasicDriver:
     def __init__(self, dag: RayServeDAGHandle):
         self.dag = dag
